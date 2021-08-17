a264b0eb46501b8b0c03a54a9842c6dc0fbb4609
Sherwin <warren.tripleonetech@gmail.com>
2021-08-17 19:58:29 +0800

    new commit

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
+import firebase from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCBBuTndT9m1goO2zPdgMG-dYJSkOUMKls",
